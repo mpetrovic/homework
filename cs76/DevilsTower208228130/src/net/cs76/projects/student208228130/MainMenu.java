a732b50a63faee6995111533576dc0ee0d2557d2
mpetrovic <mpetrovic@guneva.net>
2012-03-08 23:50:12 -0500

    Devils Tower initial commit.

---
+++ cs76/DevilsTower208228130/src/net/cs76/projects/student208228130/MainMenu.java
@@ -0,0 +1,20 @@
+package net.cs76.projects.student208228130;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainMenu extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+}
